a0436756bd3a54151a8e01122dc0e1e54323667c
Steven Evan <steven.evan999@gmail.com>
2023-01-19 16:59:15 +0700

    feat: add selected nav

--- src/screens/hoc/Layout/MainLinks.tsx
+++ src/screens/hoc/Layout/MainLinks.tsx
@@ -1,33 +1,53 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
+import { IconBooks, IconShoppingCart } from "@tabler/icons";
 import {
-  IconBooks, IconShoppingCart
-} from "@tabler/icons";
-import { ThemeIcon, UnstyledButton, Group, Text } from "@mantine/core";
+  ThemeIcon,
+  UnstyledButton,
+  Group,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
+import { useRouter } from "next/router";
 
 interface MainLinkProps {
   icon: ReactNode;
   color: string;
   label: string;
+  navLink: string;
 }
 
-function MainLink({ icon, color, label }: MainLinkProps) {
+function MainLink({ icon, color, label, navLink }: MainLinkProps) {
+  const theme = useMantineTheme();
+  const router = useRouter();
+  const handleClick = () => {
+    void router.push(navLink);
+  };
+
+  const isSelected = router.pathname === navLink;
+  const backgroundColor: string = isSelected
+    ? theme.colorScheme === "dark"
+      ? theme.colors.dark[7]
+      : theme.colors.gray[1]
+    : "inherit";
+
   return (
     <UnstyledButton
-      sx={(theme) => ({
+      sx={{
         display: "block",
         width: "100%",
         padding: theme.spacing.xs,
         borderRadius: theme.radius.sm,
         color:
           theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-
+        backgroundColor,
         "&:hover": {
           backgroundColor:
             theme.colorScheme === "dark"
               ? theme.colors.dark[6]
               : theme.colors.gray[0],
         },
-      })}
+      }}
+      onClick={handleClick}
     >
       <Group>
         <ThemeIcon color={color} variant="light">
@@ -45,8 +65,14 @@ const data = [
     icon: <IconBooks size={16} />,
     color: "blue",
     label: "Products",
+    navLink: "/products",
   },
-  { icon: <IconShoppingCart size={16} />, color: "teal", label: "Carts" },
+  {
+    icon: <IconShoppingCart size={16} />,
+    color: "teal",
+    label: "Carts",
+    navLink: "/carts",
+  },
 ];
 
 export function MainLinks() {
