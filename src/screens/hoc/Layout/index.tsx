5f01717fd4b8681eb18bb84ddd26f8be118900dd
Steven Evan <steven.evan999@gmail.com>
2023-01-19 23:28:52 +0700

    feat: add header title and github link

--- src/screens/hoc/Layout/index.tsx
+++ src/screens/hoc/Layout/index.tsx
@@ -6,11 +6,16 @@ import {
   MediaQuery,
   Flex,
   useMantineTheme,
+  Title,
+  ActionIcon,
+  Group,
 } from "@mantine/core";
 import type { ReactElement } from "react";
 import { MainLinks } from "./MainLinks";
 import { User } from "./User";
 import { useDisclosure } from "@mantine/hooks";
+import { IconBrandGithub } from "@tabler/icons";
+import Link from "next/link";
 
 type iLayoutProps = {
   children: ReactElement;
@@ -39,17 +44,29 @@ const Layout = ({ children }: iLayoutProps) => {
       }
       navbarOffsetBreakpoint="sm"
       header={
-        <Header height={{ base: 50, md: 70 }} p="md">
-          <Flex align="center" style={{ height: "100%" }}>
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={handler.toggle}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
+        <Header height={{ base: 50, md: 70 }} py="md" px="lg">
+          <Flex
+            align="center"
+            justify={"space-between"}
+            style={{ height: "100%" }}
+          >
+            <Group>
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={handler.toggle}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+              <Title order={3}>DealJob Test</Title>
+            </Group>
+            <Link href="https://github.com/stevenevan/dealjob-test" passHref>
+              <ActionIcon variant="outline" size="lg">
+                <IconBrandGithub />
+              </ActionIcon>
+            </Link>
           </Flex>
         </Header>
       }
